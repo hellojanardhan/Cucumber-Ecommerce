8696cf81472bbf9999ce9b1957c63d7e1c119dbc
janardhan <125296523+hellojanardhan@users.noreply.github.com>
2023-09-11 22:59:59 +0530

    Add files via upload

---
+++ test/java/pageObjects/LoginPage.java
@@ -0,0 +1,41 @@
+package pageObjects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage {
+	WebDriver driver;
+	public LoginPage(WebDriver d) {
+		driver=d;
+		PageFactory.initElements(d, this);
+	}
+	
+	@FindBy(name = "Email")
+	WebElement Email;
+	
+	@FindBy(name = "Password")
+	WebElement Password;
+	
+	@FindBy(xpath = "//button[@type='submit']")
+	WebElement loginBtn;
+	
+	@FindBy(xpath = "/html/body/div[3]/nav/div/ul/li[3]/a")
+	WebElement logoutBtn;
+	public void setEmail(String email) {
+		Email.clear();
+		Email.sendKeys(email);
+	}
+	public void setPassword(String password) {
+		Password.clear();
+		Password.sendKeys(password);
+	}
+	public void Login() {
+		loginBtn.click();
+	}
+	
+	public void Logout() {
+		logoutBtn.click();
+	}
+}
